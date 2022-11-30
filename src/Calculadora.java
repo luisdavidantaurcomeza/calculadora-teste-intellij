c3ef90b7acb7e0c9ff0fe56c3a0be92601d972aa
Luis David <luisdavid_204@hotmail.com>
2022-11-30 19:52:20 -0300

    first commit

---
+++ src/Calculadora.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a,b;
+        System.out.println("Digite o primeiro valor");
+        a=scan.nextInt();
+        System.out.println("Digite o segundo valor");
+        b=scan.nextInt();
+
+        int somar = somar(a, b);
+        int subtrair = subtrair(a,b);
+        int multiplicar = multiplicar(a,b);
+        double dividir = dividir(a,b);
+
+        System.out.println("somar"+somar);
+        System.out.println("sub: "+subtrair);
+        System.out.println("div: "+dividir);
+        System.out.println("mult: "+multiplicar);
+
+    }
+    public static int somar (int a, int b){
+        return a+b;
+    }
+    public static int subtrair (int a, int b){
+        return a-b;
+    }
+    public static double dividir (int a, int b){
+        return a/b;
+    }
+    public static int multiplicar (int a, int b){
+        return a*b;
+    }
+}
